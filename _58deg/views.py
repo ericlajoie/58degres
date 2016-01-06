85a6bbad156230a3715ed2e2e8f6b2c08b2c483f
Eric JEANGIRARD <eric.jeangirard@leboncoin.loc>
2016-01-06 14:18:33 +0100

    txt details

--- _58deg/views.py
+++ _58deg/views.py
@@ -16,14 +16,15 @@ def detail(request, artist, art_id):
 	
 	#img_list = os.listdir("static/_58deg/")
 	img_list = os.listdir("mysite/static/%s/%s/" % (artist,art_id))
-	return render(request, 'polls/detail.html', {'artist':artist, 'art_id': art_id, 'img_list':img_list})
+	detail_text = open("mysite/static/%s/%s.txt" % (artist,art_id)).read()
+	return render(request, 'polls/detail.html', {'artist':artist, 'art_id': art_id, 'img_list':img_list, 'detail_text':detail_text})
 	#return HttpResponse("You're looking at <h1> %s </h1>" % artist)
 
 def listing(request, artist):
 	file_list = os.listdir("mysite/static/%s/" %artist)
 	project_list = []
 	for p in file_list:
-		if (not p.endswith(".jpg")):
+		if (not p.endswith(".jpg") and not p.endswith(".txt")):
 			project_list.append(p)
 	return render(request, 'polls/listing.html', {'artist':artist, 'project_list':project_list})
 	
