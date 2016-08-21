0a16e68b424bf49c2f818cc7d3598bd2c112f035
eric <eric@eric.fr>
2016-08-21 16:00:01 +0200

    new version after discussion

--- _58deg/views.py
+++ _58deg/views.py
@@ -12,8 +12,8 @@ def atelier(request):
 def merci(request):
 	return render(request, 'polls/merci.html')
 	
-def en_ce_moment(request):
-	return render(request, 'polls/en-ce-moment.html')
+#def en_ce_moment(request):
+#	return render(request, 'polls/en-ce-moment.html')
 	
 def contact(request):
 	return render(request, 'polls/contact.html')
@@ -22,18 +22,44 @@ def les_copains(request):
 	return render(request, 'polls/les-copains.html')
 	
 def detail(request, artist, art_id):
-	img_list = os.listdir("mysite/static/%s/%s/" % (artist,art_id))
-	detail_text = open("mysite/static/%s/%s.txt" % (artist,art_id)).read()
+	file_list = os.listdir("mysite/static/%s/%s/" % (artist,art_id))
+	img_list = []
+	for p in file_list:
+		if ((p.endswith(".jpg")) and not(p.startswith('.'))):
+			img_list.append(p)
+	detail_text_str = open("mysite/static/%s/%s.txt" % (artist,art_id)).read()
+	
+	detail_text = detail_text_str.split('|')
+	
+	for k in range (0,len(detail_text)):
+		detail_text[k] = detail_text[k].replace("â€™", "'").encode('iso-8859-1')
+		
+		
 	return render(request, 'polls/detail.html', {'artist':artist, 'art_id': art_id, 'img_list':img_list, 'detail_text':detail_text})
-	#return HttpResponse("You're looking at <h1> %s </h1>" % artist)
-
-def listing(request, artist):
+	#return HttpResponse("You're looking at <h1> %s </h1>" % artist)	
+	
+def listing(request, artist="58deg"):
 	file_list = os.listdir("mysite/static/%s/" %artist)
 	project_list = []
 	for p in file_list:
-		if (not p.endswith(".jpg") and not p.endswith(".txt")):
+		if (not p.endswith(".jpg") and not p.endswith(".txt") and not "Thumbs" in p):
 			project_list.append(p)
+	project_list.reverse()
 	return render(request, 'polls/listing.html', {'artist':artist, 'project_list':project_list})
+	
+def en_ce_moment(request):
+	file_list = os.listdir("mysite/static/en-ce-moment/")
+	event_list = []
+	for p in file_list:
+		if (p.endswith(".jpg") ):
+			event_name = p.replace('.jpg','')
+			
+			txt_str = open("mysite/static/en-ce-moment/%s.txt" % (event_name)).read()
+			event_str = txt_str.encode('iso-8859-1')
+			event_list.append([event_name, event_str])
+			
+	event_list.reverse()
+	return render(request, 'polls/en-ce-moment.html', {'event_list':event_list})
 
 def shop_listing(request, shop_category):
 	file_list = os.listdir("mysite/static/%s/" %shop_category)
@@ -46,6 +72,6 @@ def shop_listing(request, shop_category):
 	
 def shop_detail(request, shop_category, art_id):
 	img_list = os.listdir("mysite/static/%s/%s/" % (shop_category,art_id))
-	detail_text = open("mysite/static/%s/%s.txt" % (shop_category,art_id)).read()
+	detail_text = open("mysite/static/%s/%s.txt" % (shop_category,art_id)).read().encode('iso-8859-1')
 	return render(request, 'polls/shop_detail.html', {'shop_category':shop_category, 'art_id': art_id, 'img_list':img_list, 'detail_text':detail_text})
 	#return HttpResponse("You're looking at <h1> %s </h1>" % artist)
